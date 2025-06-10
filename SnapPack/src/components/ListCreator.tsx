e2cf3cf43e4cfcaec7572b504eb36bcf785e90b6
MAX <1992mmaximm1992@gmail.com>
2025-06-10 16:41:57 +0300

    First

--- SnapPack/src/components/ListCreator.tsx
+++ SnapPack/src/components/ListCreator.tsx
@@ -2,27 +2,31 @@ import {useState} from "react";
 import './ListCreator.css';
 import HoverButton from "./HoverButton.tsx";
 import HoverButtonCampfire from "./HoverButtonCampfire.tsx";
+import SubjectCreator from './SubjectCreator.tsx';
+
 function ListCreator() {
-    const [inputValue, setInputValue] = useState("");
-    const [lists, setDivs] = useState([]);
+    const [inputValue, setInputValue] = useState(""); //состояние для введенного значения
+    const [lists, setLists] = useState([]); //состояние для списка листов
 
-    // Добавление нового div
-    const handleAddDiv = () => {
-        if (!inputValue.trim()) return; // Игнорируем пустые значения
-
-        setDivs([...lists, inputValue]);
-        setInputValue(""); // Очищаем поле ввода
+    //-------- Добавление нового list
+    const handleAddList = () => {
+        if (!inputValue.trim()) return; // удаляем пустые значения в начале и конце строки, если после этого строка осталась пустой функция завершается
+        setLists([...lists, inputValue]); //обновляем состояние,добавляя новый элемент 'inputValue'
+        setInputValue(""); // Очищаем поле ввода (обнуляем состояние)
     };
 
-    // Удаление div по индексу
-    const removeList = (index) => {
-        setDivs(lists.filter((_, i) => i !== index));
+    //--------- Удаление list по индексу
+    const removeList = (indexToRemove) => {
+        const newLists = lists.filter((item, currentIndex) => {
+            return currentIndex !== indexToRemove;
+        });
+        setLists(newLists);
     };
 
-    // Обработка нажатия Enter
+    //---------- Обработка нажатия Enter
     const handleKeyPress = (e) => {
         if (e.key === "Enter") {
-            handleAddDiv();
+            handleAddList();
         }
     };
 
@@ -36,27 +40,34 @@ function ListCreator() {
                     onKeyPress={handleKeyPress}
                     placeholder="List name"
                 />
-                <button onClick={handleAddDiv}>Создать div</button>
+                <button onClick={handleAddList}>Create new list</button>
             </div>
 
             <div className="lists-container">
                 {lists.map((text, index) => (
                     <div key={index} className="custom-list">
-                        <span>{text}</span>
-                        <button>
-                            <HoverButton/>
-                        </button>
+                        <div className="lists-input-container">
+                            <span>{text}</span>
+                            <div className='buttons-conteiner'>
+                                <button
+                                    className = 'add-btn'>
 
-                        <button
-                            onClick={() => removeList(index)}
-                            className="delete-btn"
-                        >
-                            <HoverButtonCampfire/>
-                        </button>
+                                <HoverButton/>
+                                </button>
+                                <button
+                                    onClick={() => removeList(index)}
+                                    className="delete-btn">
+                                    <HoverButtonCampfire/>
+                                </button>
+                            </div>
+                        </div>
+                        <div><SubjectCreator/></div>
                     </div>
                 ))}
+
             </div>
         </div>
+
     );
 }
 
---
+++ SnapPack/src/components/SubjectCreator.tsx
@@ -0,0 +1,63 @@
+import {useState} from "react";
+
+import HoverButtonCampfire from "./HoverButtonCampfire.tsx";
+import './SubjectCreator.css';
+function SubjectCreator() {
+    const [inputSubjectValue, setInputSubjectValue] = useState(""); //состояние для введенного значения
+    const [subjects, setSubjects] = useState([]); //состояние для списка листов
+
+    //-------- Добавление нового list
+    const handleAddSubj = () => {
+        if (!inputSubjectValue.trim()) return; // удаляем пустые значения в начале и конце строки, если после этого строка осталась пустой функция завершается
+        setSubjects([...subjects, inputSubjectValue]); //обновляем состояние,добавляя новый элемент 'inputSubjectValue'
+        setInputSubjectValue(""); // Очищаем поле ввода (обнуляем состояние)
+    };
+
+    //--------- Удаление list по индексу
+    const removeSubj = (indexToRemove) => {
+        const newSubjects = subjects.filter((item, currentIndex) => {
+            return currentIndex !== indexToRemove;
+        });
+        setLists(newSubjects);
+    };
+
+    //---------- Обработка нажатия Enter
+    const handleKeySubjPress = (e) => {
+        if (e.key === "Enter") {
+            handleAddSubj();
+        }
+    };
+
+    return (
+        <div className="div-creator">
+            <div className="controls">
+                <input
+                    type="text"
+                    value={inputSubjectValue}
+                    onChange={(e) => setInputSubjectValue(e.target.value)}
+                    onKeyPress={handleKeySubjPress}
+                    placeholder="List name"
+                />
+                <button onClick={handleAddSubj}>Create new subject</button>
+            </div>
+
+            <div className="subjects-container">
+                {subjects.map((text, index) => (
+                    <div key={index} className="custom-subject-list">
+                        <span>{text}</span>
+                        <div className='buttons-conteiner'>
+                            <button
+                                onClick={() => removeSubj(index)}
+                                className="delete-btn">
+                                <HoverButtonCampfire/>
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+
+    );
+}
+
+export default SubjectCreator;
